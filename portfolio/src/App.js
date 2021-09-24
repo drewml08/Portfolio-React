4c661007caa1bc6a14fbe38fc799a4f31f6b8a97
Mike Drew <drewml08@gmail.com>
2021-09-24 01:33:29 -0400

    wired up most styling and logic for header of portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
    
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
             <div>
                 <Switch>
                     <Route exact path="/" component={Home} />
+                    <Route exact path="/projects" component={Projects} />
                 </Switch>    
             </div>
             <Footer />
--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./style.css";
+import profileImage from "./profile_large.png";
 
 function Header() {
+    const history = useHistory();
+
+    const goToHome = () => {
+        history.push("/");
+    };
+
+    const goToProjects = () => {
+        history.push("/projects");    
+    };
+
   return (
     <div className="header">
-      <span>Header Wireframe</span>
+      <div className="d-flex">
+        <div className="profile-container p-2">
+          <img className="profile" src={profileImage} alt="" />
+        </div>
+        <div className="p-2">
+          <h1 className="name">Mike Drew</h1>
+          <h3 className="tagline ">Aspiring Software Developer</h3>
+        </div>
+        <div className="p-2 ml-auto">
+        <button className="btn btn-primary p-2" onClick={goToHome}>Home</button>
+        <button className="btn btn-primary p-2" onClick={goToProjects}>Projects</button>
+        </div>
+      </div>
+     
+      
     </div>
   );
 }
